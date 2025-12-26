81844ca6439331e28f03f298510e3d8fc7a8c5ed
Hồ Xuân Thái <thai.hx04@gmail.com>
2025-12-26 11:06:54 +0700

    fix api health data

--- src/main/java/com/hoxuanthai/be/lastdance/entity/HealthDataId.java
+++ src/main/java/com/hoxuanthai/be/lastdance/entity/HealthDataId.java
@@ -6,11 +6,12 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class HealthDataId implements Serializable {
-    private Long id;
+    private UUID id;
     private LocalDateTime timestamp;
 }
